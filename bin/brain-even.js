70fd6b4bc735a39acd3047f8797ba93d5b31cded
Marworld <marworld@mail.ru>
2020-06-06 13:35:25 +0300

    change29

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -5,9 +5,9 @@ import {randomNumber, randomNumber2, randomNumber3, checkResponse, checkCorrect}
 console.log('Welcome to the Brain Games!');
 console.log(`Hello, ${dialogue('May I have your name?: ')}`);
 console.log('Answer "yes" if the number is even, otherwise answer "no"');
-console.log('Question: ' + randomNumber);
+console.log(`Question: ${randomNumber}`);
 console.log(checkResponse(dialogue('Your answer: '), randomNumber));
-console.log('Question: ' + randomNumber2);
+console.log(`Question: ${randomNumber2}`);
 console.log(checkResponse(dialogue('Your answer: '), randomNumber2));
-console.log('Question: ' + randomNumber3);
+console.log(`Question: ${randomNumber3}`);
 console.log(checkCorrect(checkResponse(dialogue('Your answer: '), randomNumber3)));
