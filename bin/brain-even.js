7ba39690b8fb6c017aad87157cf08706eae24da5
Marworld <marworld@mail.ru>
2020-06-06 12:26:24 +0300

    new game1

---
+++ bin/brain-even.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import dialogue from '../index.js';
+import {randomNumber, randomNumber2, randomNumber3} from '../src/cli.js';
+import {checkResponse} from '../src/cli.js';
+import {checkCorrect} from '../src/cli.js';
+
+console.log('Welcome to the Brain Games!');
+console.log(`Hello, ${dialogue('May I have your name?: ')}`);
+console.log('Answer "yes" if the number is even, otherwise answer "no"');
+console.log('Question: ' + randomNumber);
+console.log(checkResponse(dialogue('Your answer: '), randomNumber));
+console.log('Question: ' + randomNumber2);
+console.log(checkResponse(dialogue('Your answer: '), randomNumber2));
+console.log('Question: ' + randomNumber3);
+console.log(checkCorrect(checkResponse(dialogue('Your answer: '), randomNumber3)));
