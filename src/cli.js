fca25a67744c8fd1f611c115dbb97dc642552e9e
Marworld <marworld@mail.ru>
2020-06-06 14:29:41 +0300

    change31

--- src/cli.js
+++ src/cli.js
@@ -6,17 +6,18 @@ export const randomNumber = Math.round(Math.random() * 100);
 export const randomNumber2 = Math.round(Math.random() * 100);
 export const randomNumber3 = Math.round(Math.random() * 100);
 export const checkResponse = (str, num) => {
-if ((num % 2 === 0 && str === 'yes') || (num % 2 != 0 && str === 'no')) {
-return 'Correct';
-}
-else if (num % 2 === 0 && str != 'yes') {
-return '"no" is wrong answer ;(. Correct answer was "yes"';
-} else {
-return '"yes" is wrong answer ;(. Correct answer was "no"';
-}
+  if ((num % 2 === 0 && str === 'yes') || (num % 2 !== 0 && str === 'no')) {
+    return 'Correct';
+  }
+  if (num % 2 === 0 && str !== 'yes') {
+    return '"no" is wrong answer ;(. Correct answer was "yes"';
+  } else {
+    return '"yes" is wrong answer ;(. Correct answer was "no"';
+  }
 };
 export const checkCorrect = (str) => {
-if (str === 'Correct') {
-return 'Congratulations!';
-}
+  if (str === 'Correct') {
+    return 'Congratulations!';
+  }
 };
+
