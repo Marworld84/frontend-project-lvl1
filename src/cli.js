f0934a303cb824378765b4a4c26b7381d8de86a4
Marworld <marworld@mail.ru>
2020-06-06 13:23:52 +0300

    change27

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,22 @@
 import readlineSync from 'readline-sync';
 
-const name = (str) => readlineSync.question(str);
-export default name;
+const dialogue = (str) => readlineSync.question(str);
+export default dialogue;
+export const randomNumber = Math.round(Math.random() * 100);
+export const randomNumber2 = Math.round(Math.random() * 100);
+export const randomNumber3 = Math.round(Math.random() * 100);
+export const checkResponse = (str, num) => {
+if ((num % 2 === 0 && str === 'yes') || (num % 2 != 0 && str === 'no')) {
+return 'Correct';
+}
+else if (num % 2 === 0 && str != 'yes') {
+return '"no" is wrong answer ;(. Correct answer was "yes"';
+} else {
+return '"yes" is wrong answer ;(. Correct answer was "no"';
+}
+};
+export const checkCorrect = (str) => {
+if (str === 'Correct') {
+return 'Congratulations!';
+}
+};
